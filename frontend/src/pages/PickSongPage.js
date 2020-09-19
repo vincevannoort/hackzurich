c6cb982174f9bdad0c6910a88fc871c1b430fac9
Vince van Noort <vincevannoort@gmail.com>
2020-09-20 01:47:42 +0200

    pick song page implementation

--- frontend/src/pages/PickSongPage.js
+++ frontend/src/pages/PickSongPage.js
@@ -1,5 +1,5 @@
 // @ts-check
-import React, { useEffect, useState,  useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import LinearGradient from 'react-native-linear-gradient';
 
 import {
@@ -27,145 +27,96 @@ import querystring from 'querystring';
 import { debounce } from "lodash";
 import axios from 'axios';
 
-
-async function downloadAndPlayUrl(varurl) {
-    RNFetchBlob
-.config({
-// add this option that makes response data to be stored as a file,
-// this is much more performant.
-fileCache : true,
-})
-.fetch('GET', varurl, {
-//some headers ..
-})
-.then((res) => {
-// the temp file path
-console.log('The file saved to ', res.path())
-Sound.setCategory('Playback');
-
-// Load the sound file 'whoosh.mp3' from the app bundle
-// See notes below about preloading sounds within initialization code below.
-var whoosh = new Sound(res.path(), Sound.MAIN_BUNDLE, (error) => {
-if (error) {
-console.log('failed to load the sound', error);
-return;
-}
-// loaded successfully
-console.log('duration in seconds: ' + whoosh.getDuration() + 'number of channels: ' + whoosh.getNumberOfChannels());
-
-// Play the sound with an onEnd callback
-whoosh.play((success) => {
-if (success) {
-  console.log('successfully finished playing');
-} else {
-  console.log('playback failed due to audio decoding errors');
-}
-});
-});
-})
-
-}
-
-function SearchSpotifyAndPlay(s) {
+async function searchSpotifyAndPlay(s) {
     const clientsecret = 'Basic MzgwNWI2ZTJlNjI4NDZkYmE0YWY2N2E0MDU3OTVmYjQ6NjkwZDE4ODczYzEzNDUxNGJlMDZjOWQ3Mjc3ODRiYzM=';
+    let accessToken = null;
 
-    var acc_token = 'QQ';
-  axios.post( 'https://accounts.spotify.com/api/token', 
-        querystring.stringify({
-        grant_type: 'client_credentials'
+    /** request access token */
+    try {
+        const response = await axios.post('https://accounts.spotify.com/api/token',
+            querystring.stringify({
+                grant_type: 'client_credentials'
+            }), {
+            headers: {
+                'Authorization': clientsecret,
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        });
+        accessToken = response.data.access_token
+    } catch (error) {
+        console.log('error requesting access token', error.message)
+    }
+
+    try {
+        if (accessToken == null) throw new Error("No access token found")
+        const responseGet = await axios({
+            method: 'get',
+            url: 'https://api.spotify.com/v1/search',
+            params: {
+                q: s,
+                type: 'track',
+                limit: 5
+            },
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
         })
-      , {
-      headers: {
-        'Authorization': clientsecret,
-        'Accept':'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded',
-      }
-    })
-  .then(function (response) {
-    console.log('SPOTIFY SUCCES'); 
-    console.log(response);
-    console.log('Acces token');
-
-    console.log(response.data.access_token);
-    acc_token = response.data.access_token;
-    console.log('AAAA')
-    console.log('AAA \n')
-    console.log('hALLO ZEG:' + acc_token);
-
-    axios({method:'get',
-    url: 'https://api.spotify.com/v1/search', 
-    params: {
-  q: 'ANdromedik',
-  type: 'track',
-  limit: 5
-    },
-  headers: {
-    'Authorization': 'Bearer ' + acc_token,
-    'Accept':'application/json',
-    'Content-Type': 'application/x-www-form-urlencoded',
-  }
-})
-.then(function (response) {
-console.log('Search SUCCES');
-console.log(response);
-
-var plyuri = response.data.tracks.items[0].preview_url;
-console.log(plyuri);
-downloadAndPlayUrl(plyuri);
-console.log('Search token');
-
-//   console.log(response.data.access_token);
-//   acc_token = response.data.acces_token;
-//   console.log('AAAA')
-//console.log(respbody['access_token']);
-})
-.catch(function (error) {
-  console.log('Search ERRRROR');
-console.log(error);
-console.log(error.response.data);
-});
-
-    
-    //console.log(respbody['access_token']);
-  })
-  .catch(function (error) {
-      console.log('SPOTIFY ERRRROR');
-    console.log(error);
-    console.log(error.response.data);
-  });
+        return responseGet.data.tracks.items
+    } catch (error) {
+        console.log(accessToken)
+        console.log('error requesting song', error.message)
+    }
 
 }
 
 function PickSongPage() {
-
-
     const [search, setSearch] = useState('');
-    const handler = useCallback((s) => debounce(function(s){
-        console.log('handler')
-        console.log(s);
-        SearchSpotifyAndPlay(s)
-    }, 500), []);
-    function changedSearch(s) {
-        //console.log('Veranderen')
-        setSearch(s);
-        SearchSpotifyAndPlay(s);// handler(s);
-    }
-    
+    const [tracks, setTracks] = useState([]);
 
-    const greeting = 'Hello Function Component!';
+    async function searchSpotify(search) {
+        const trackList = await searchSpotifyAndPlay(search);
+        if (trackList == null || trackList == undefined) return;
+        console.log('trackList')
+        console.log(trackList)
+        setTracks(trackList)
+    }
+
+    const handler = useCallback(debounce(() => {
+        searchSpotify(search);
+    }, 500), [search]);
+
+    function changedSearch(s) {
+        setSearch(s);
+        handler()
+    }
 
     return (
         <View>
             <Text>PickSongPage</Text>
-<View>
-<SearchBar
-        placeholder="Type Here..."
-        onChangeText={changedSearch }
-        value={search}
-      />
-</View>
+            <View>
+                <SearchBar
+                    placeholder="Type Here..."
+                    onChangeText={changedSearch}
+                    value={search}
+                />
+                <ScrollView>
+                    {
+                        tracks.map(track => {
+                            return (
+                                <View key={track.id}>
+                                    <Text>{track.name}</Text>
+                                </View>
+
+                            )
+                        })
+                    }
+                </ScrollView>
+            </View>
         </View>
-        
+
     );
 }
 
